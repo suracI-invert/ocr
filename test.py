0400010b60721d4977b5b88bf54a4ed0ff919529
suracI-invert <koolsha123@gmail.com>
2023-09-07 22:01:34 +0700

    add requirements

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
-from torchvision import transforms
-from torch.nn.functional import sigmoid
-import random
+from transformers import AutoImageProcessor, SwinModel
+from PIL import Image
 import torch
 
+image = Image.open('./data/new_train/train_img_0.jpg')
 
-lst = random.sample([
-    transforms.GaussianBlur(kernel_size= 1, sigma= (0.1, 1)),
-    transforms.ColorJitter(contrast= (0.4, 0.6), hue= (-0.5, 0.5), saturation= (0, 10)),
-    transforms.RandomInvert(0.25),
-    transforms.RandomSolarize(128, 0.5)
-], k= 2)
+image_processor = AutoImageProcessor.from_pretrained("microsoft/swin-tiny-patch4-window7-224")
+model = SwinModel.from_pretrained("microsoft/swin-tiny-patch4-window7-224")
 
-transforms_lst = [transforms.ToTensor()] + lst + [transforms.Resize((10, 20))]
-print(transforms.Compose(transforms_lst))
+inputs = image_processor(image, return_tensors="pt")
+
+with torch.no_grad():
+    embedding = model(**inputs).last_hidden_state
+
+print(embedding.shape)
+
